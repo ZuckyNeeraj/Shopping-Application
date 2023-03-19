cced252fc0689f264cd2e3aaed0a5e43eb150ca3
Neeraj <neeraj.mahapatra@mindstix.com>
2023-03-19 23:45:41 +0530

    added basic navigation drawer layout with a header and some menu items

--- app/src/main/java/com/example/shoppingapplication/ui/homepage/HomePageActivity.kt
+++ app/src/main/java/com/example/shoppingapplication/ui/homepage/HomePageActivity.kt
@@ -1,13 +1,25 @@
 package com.example.shoppingapplication.ui.homepage
 
+/**
+ * This is the home page activity that will be providing navigation feature.
+ * As well as logout functionality is also added in this section.
+ * @author Neeraj Mahapatra
+ */
+
 import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.shoppingapplication.R
 import com.example.shoppingapplication.databinding.ActivityHomePageBinding
 import com.example.shoppingapplication.ui.auth.AuthActivity
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
@@ -17,6 +29,12 @@ private lateinit var binding: ActivityHomePageBinding
 @SuppressLint("StaticFieldLeak")
 private lateinit var logOutButton: Button
 private lateinit var auth: FirebaseAuth
+private lateinit var drawerlayout: DrawerLayout
+private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+private lateinit var navigationView: NavigationView
+private val productDisplayFrag = ProductDisplayFragment()
+private val cartFrag = AddToCartFragment()
+private val userDetailFrag = UserDetailsFragment()
 
 
 class HomePageActivity : AppCompatActivity(){
@@ -24,19 +42,88 @@ class HomePageActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding = ActivityHomePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        loadTheInitialFragments()
+
+        setCurrentFragment(productDisplayFrag)
+        drawer_func()
         logOut()
+    }
+
+    override fun onBackPressed() {
+        drawerlayout = binding.drawerLayout
+        if(drawerlayout.isDrawerOpen(GravityCompat.START)){
+            drawerlayout.closeDrawer(GravityCompat.START)
+        }else {
+            super.onBackPressed()
+        }
+    }
+
+    /**
+     * Navigation drawer function that will be triggered on clicking the
+     * hamburger image.
+     * @return Navigation Drawer
+     */
+    private fun drawer_func() {
+        // Set the toolbar as the action bar
+        val toolbar = binding.hamburgerMenu
+
+        // Set a click listener on the navigation icon to open the drawer
+        toolbar.setOnClickListener {
+            drawerlayout.openDrawer(GravityCompat.START)
+        }
+        drawerlayout = binding.drawerLayout
+        navigationView = binding.navigationView
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerlayout, R.string.nav_open, R.string.nav_close)
+
+        drawerlayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_prod_display -> {
+                    drawerlayout.closeDrawer(GravityCompat.START)
+                    setCurrentFragmentOnCLick(productDisplayFrag)
+                    true
+                }
+                R.id.nav_cart -> {
+                    drawerlayout.closeDrawer(GravityCompat.START)
+                    setCurrentFragmentOnCLick(cartFrag)
+                    true
+                }
+                R.id.nav_user_detail -> {
+                    drawerlayout.closeDrawer(GravityCompat.START)
+                    setCurrentFragmentOnCLick(userDetailFrag)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
     }
 
 
     /**
      * This method will load the very first fragment.
      */
-    private fun loadTheInitialFragments() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.recyclerViewFrameLayout, ProductDisplayFragment())
-            .commit()
-    }
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.recyclerViewFrameLayout, fragment)
+            commit()
+        }
+
+    private fun setCurrentFragmentOnCLick(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.recyclerViewFrameLayout, fragment)
+            addToBackStack(null)
+            commit()
+        }
 
 
     /**
